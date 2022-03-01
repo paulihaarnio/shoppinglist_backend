49b274c5cc03a04c52b72c5058585a378656dc0b
paulihaarnio <pauli.haarnio@gmail.com>
2022-03-01 13:09:47 +0200

    tehty

---
+++ add.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'inc/headers.php';
+require_once 'inc/functions.php';
+$input = json_decode(file_get_contents('php://input'));
+$description = filter_var($input->description, FILTER_SANITIZE_SPECIAL_CHARS);
+$amount = filter_var($input->amount, FILTER_SANITIZE_SPECIAL_CHARS);
+try {
+    $db = openDB();
+    $query = $db->prepare('insert into item(description, amount) values (:description, :amount)');
+    $query->bindValue(':description', $description, PDO::PARAM_STR,':amount', $amount, PDO::PARAM_INT);
+    $query->execute();
+
+    header('HTTP/1.1 200 OK');
+    $data = array('id' => $db->lastInsertId(), 'description' => $description,'amount'=>$amount);
+    print json_encode($data);
+} catch (PDOException $pdoex) {
+    returnError($pdoex);
+}
--- database.sql
+++ database.sql
@@ -2,10 +2,10 @@ create database shoppingList;
 
 use shoppingList;
 
-create table item{
+create table item(
     id int primary key auto_increment,
     description varchar(255) not null,
     amount smallint unsigned not null
-};
+);
 
-insert into into item (description, amount) values('Test item,1');
+insert into item (description, amount) values('Test item',1);
